44c90424a400a165f02f1abb625730a667ac2ea0
Dasha <dasha.kotova.mail@gmail.com>
2021-07-07 18:06:53 -0400

    modify plant context

--- client/src/context/PlantContext.js
+++ client/src/context/PlantContext.js
@@ -3,5 +3,20 @@ import React, { useState, createContext } from "react";
 export const PlantsContext = createContext();
 
 export const PlantsContextProvider = (props) => {
-  
-}
+
+  const [plants, setPlants] = useState([]);
+
+  const addPlants = (plant) => {
+    setPlants([...plants, plant]);
+  };
+
+  return(
+    <PlantsContext.Provider value={{
+      plants,
+      setPlants,
+      addPlants
+    }}>
+      {props.children}
+    </PlantsContext.Provider>
+  )
+};
