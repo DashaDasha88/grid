695c8e199a5abd6e63e157c061c46eb2de0110e7
Dasha <dasha.kotova.mail@gmail.com>
2021-07-08 21:06:07 -0400

    fill in the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      Header
+      <h1 className="font-weight-light display-1 text-center">Grid Exercise</h1>
+      <p>I'm making this little page in order to practice making a grid in React.</p>
     </div>
   )
 }
